87303ae6c6934dc74481862843189ff76a30de83
melobit <helmaeb26@gmail.com>
2024-03-11 20:53:34 +0330

    final

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "../UI/Card";
+import styles from "./UsersList.module.css";
+
+const UserList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.items.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old )
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+export default UserList;
